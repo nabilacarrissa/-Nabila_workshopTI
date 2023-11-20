7173b95831b811c7083afd3d404e1bbb7d6da6bf
Mohammad Firdausin <mfirdausin227@gmail.com>
2023-11-20 14:56:02 +0700

    Push Workshop2023

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Data Diri</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h1 class="text-center">Data Diri</h1>
+                <table class="table table-bordered">
+                    <tr>
+                        <th>Nama</th>
+                        <td>Nabila Carrissa Dewi</td>
+                    </tr>
+                    <tr>
+                        <th>NIM</th>
+                        <td>234311021</td>
+                    </tr>
+                    <tr>
+                        <th>Prodi</th>
+                        <td>Teknologi Rekayasa Perangkat Lunak</td>
+                    </tr>
+                    <tr>
+                        <th>Kelas</th>
+                        <td>1A</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
